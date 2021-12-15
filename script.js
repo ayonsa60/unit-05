855831a09b08847f2730be0be2a693b697ea8f7f
ayonsa60 <ayonsa60@gmail.com>
2021-12-14 19:30:15 -0600

    corrected feature that saves events when refreshed

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ $('#time').each(function(){
 });
 
 
-$('#9am .description').val(localStorage.getItem('9am'));
-$('#10am .description').val(localStorage.getItem('10am'));
-$('#11am .description').val(localStorage.getItem('11am'));
-$('#12pm .description').val(localStorage.getItem('12pm'));
-$('#1pm .description').val(localStorage.getItem('1pm'));
-$('#2pm .description').val(localStorage.getItem('2pm'));
-$('#3pm .description').val(localStorage.getItem('3pm'));
-$('#4pm .description').val(localStorage.getItem('4pm'));
-$('#5pm .description').val(localStorage.getItem('5pm'));
+$('#9am .event').val(localStorage.getItem('9am'));
+$('#10am .event').val(localStorage.getItem('10am'));
+$('#11am .event').val(localStorage.getItem('11am'));
+$('#12pm .event').val(localStorage.getItem('12pm'));
+$('#1pm .event').val(localStorage.getItem('1pm'));
+$('#2pm .event').val(localStorage.getItem('2pm'));
+$('#3pm .event').val(localStorage.getItem('3pm'));
+$('#4pm .event').val(localStorage.getItem('4pm'));
+$('#5pm .event').val(localStorage.getItem('5pm'));
